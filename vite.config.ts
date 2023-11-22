a601fe71d71e96dbb5f602a6f5e98bdd1337f2f2
hhui64 <907322015@qq.com>
2023-11-22 17:29:17 +0800

    chore: support  vite@v5

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: '0.0.0.0'
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(fileURLToPath(import.meta.url), './src')
+    }
   },
   plugins: [react()]
 })
