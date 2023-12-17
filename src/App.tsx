a5da524bf3ddd0680a2ee64229af4f245610170f
hhui64 <907322015@qq.com>
2023-12-17 20:44:07 +0800

    Remove unused dependencies and update button element

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.less'
-import { Button } from 'antd'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -19,7 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>count is {count}</Button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
