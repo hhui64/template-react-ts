bf9ff80589dd08b776f4af3b2012a15f5a9f7d48
hhui64 <907322015@qq.com>
2023-12-17 20:34:20 +0800

    Update module resolution in tsconfig.json and tsconfig.node.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+    strictPort: true
   },
   resolve: {
     alias: {
