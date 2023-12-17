76071957282d130a44e454f3c167e8c645baa8a0
hhui64 <907322015@qq.com>
2023-12-17 20:40:57 +0800

    Update ESLint configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,16 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
+  plugins: ['react-refresh'],
   extends: [
     'eslint:recommended',
-    'prettier',
-    'eslint-config-prettier',
-    'plugin:prettier/recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended'
+    'plugin:react-hooks/recommended',
+    'prettier'
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh', 'prettier'],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
   rules: {
-    'prettier/prettier': 'warn',
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
